79dfbd8034a5758573c46b52d593f67917ffd1f7
TRAVIS BENNETT <home@TRAVISs-MacBook-Pro.local>
2016-07-05 21:08:11 -0700

    added easterEgg

---
+++ assets/js/easterEgg.js
@@ -0,0 +1,13 @@
+var counter = 0;
+
+function easterEgg(){
+
+  if (counter === 4) {
+    alert("Congratulations you have discovered the TCS easterEgg!")
+  }
+  else {
+    counter ++;
+    console.log(counter);
+  }
+}
+// document.getElementById("myBtn").addEventListener("click", console.log("click"),easterEgg());
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,4 @@
-/*
-	Hyperspace by HTML5 UP
-	html5up.net | @n33co
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
+
 
 (function($) {
 
@@ -34,6 +30,7 @@
 				}, 100);
 			});
 
+
 		// Forms.
 
 			// Fix: Placeholder polyfill.
@@ -61,23 +58,23 @@
 
 		// Get the modal
 		var modal = document.getElementById('myModal');
-		
+
 		// Get the button that opens the modal
 		var btn = document.getElementById("myBtn");
 
 		// Get the <span> element that closes the modal
 		var span = document.getElementsByClassName("close")[0];
-		
-		// When the user clicks on the button, open the modal 
+
+		// When the user clicks on the button, open the modal
 		btn.onclick = function() {
 		    modal.style.display = "block";
 		}
-		
+
 		// When the user clicks on <span> (x), close the modal
 		span.onclick = function() {
 		    modal.style.display = "none";
 		}
-		
+
 		// When the user clicks anywhere outside of the modal, close it
 		window.onclick = function(event) {
 		    if (event.target == modal) {
